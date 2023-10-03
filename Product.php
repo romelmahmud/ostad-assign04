c9d46841ffd1351c25607c2427bcd7f50a3d731a
romel <romelmahmud.dev@gmail.com>
2023-10-04 00:03:36 +0600

    small fix

--- Product.php
+++ Product.php
@@ -1,28 +1,30 @@
 <?php
 
-class Product {
+class Product
+{
   private int $id;
   private string $name;
   private float $price;
 
-public function __construct($id, $name, $price)
-{
-  $this->id = $id;
-  $this->name = $name;
-  $this-> price = $price;
-}
+  public function __construct($id, $name, $price)
+  {
+    $this->id = $id;
+    $this->name = $name;
+    $this->price = $price;
+  }
 
-private function getFormattedPrice(){
-  return number_format($this->price, 2, '.', '');
-}
+  private function getFormattedPrice()
+  {
+    return number_format($this->price, 2, '.', '');
+  }
 
-public function showDetails(){
-  echo "Product Details: " . "\n";
-  echo "- ID: {$this -> id} " . "\n";
-  echo "- Name: {$this -> name} " . "\n";
-  echo "- Price: ". "$" . "{$this -> price} " . "\n";
-}
-
+  public function showDetails()
+  {
+    echo "Product Details: " . "\n";
+    echo "- ID: {$this->id} " . "\n";
+    echo "- Name: {$this->name} " . "\n";
+    echo "- Price: " . "$" . "{$this->getFormattedPrice()} " . "\n";
+  }
 }
 
 $product = new Product(1, 'T-shirt', 19.99);
