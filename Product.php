e3ee58dc95734fee2342dbedecf615c079d3d995
romel <romelmahmud.dev@gmail.com>
2023-10-03 23:41:19 +0600

    first commit

---
+++ Product.php
@@ -0,0 +1,29 @@
+<?php
+
+class Product {
+  private int $id;
+  private string $name;
+  private float $price;
+
+public function __construct($id, $name, $price)
+{
+  $this->id = $id;
+  $this->name = $name;
+  $this-> price = $price;
+}
+
+private function getFormattedPrice(){
+  return number_format($this->price, 2, '.', '');
+}
+
+public function showDetails(){
+  echo "Product Details: " . "\n";
+  echo "- ID: {$this -> id} " . "\n";
+  echo "- Name: {$this -> name} " . "\n";
+  echo "- Price: ". "$" . "{$this -> price} " . "\n";
+}
+
+}
+
+$product = new Product(1, 'T-shirt', 19.99);
+$product->showDetails();
